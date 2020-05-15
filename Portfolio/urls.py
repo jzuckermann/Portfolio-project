04c836c4d6086c905e569645151f944fb9be8209
Jonas Zuckerman <jonaszuckerman@Jonass-MacBook-Pro.local>
2020-05-14 21:39:54 -0400

    added Contact Us app

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -5,10 +5,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
 import app.views
+import contact.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("authentication.urls")),
     path('', include("app.urls")),
     path('blog/', include('blog.urls')),
+    path('ui-contact-us.html', contact.views.Contact)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class contact(models.Model):
+class Contact(models.Model):
     name = models.CharField(max_length=50)
     question1 = models.TextField(max_length=350)#Please tell us what type of even you are wanting captured?
     question2 = models.TextField(max_length=350)#What first attracted you to my work & what would you like to see in your own photographs?
